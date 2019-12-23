cccc9b5470962ee0b45f44939ead410721a40bbc
yuma yuma <yumayuma0322@gmail.com>
2019-12-23 18:44:39 +0900

    face++のテスト

---
+++ face_detect.py
@@ -0,0 +1,42 @@
+import requests
+
+def face_detect(image):
+   url = "https://api-us.faceplusplus.com/facepp/v3/detect"
+   payload = {'api_key': 'dmsZI8tMsBoceJI3hx_nPN6qy6hvM6Oq', 'api_secret': 'RyrwmZXueUjmBv79W1SlI61M3v9CqUU1HideRyrwmZXueUjmBv79W1SlI61M3v9CqUU1Hide',\
+              'image_base64': image, 'return_attributes': 'gender,age,ethnicity,beauty'}
+   try:
+       r = requests.post(url, data=payload)
+       r = r.json()
+       face_list = []
+       for face in r["faces"]:
+           faces = {}
+           if "attributes" in face:
+               faces["gender"] = face["attributes"]["gender"]["value"]
+               faces["age"] = face["attributes"]["age"]["value"]
+               # 人種を取得したい場合は以下の記述を追加する
+               # faces["ethnicity"] = face["attributes"]["ethnicity"]["value"]
+               # faces["ethnicity"] = faces["ethnicity"].replace("ASIAN", "アジア人").replace("WHITE", "白人").replace("BLACK", "黒人")
+               if faces["gender"] == "Male":
+                   faces["beauty"] = face["attributes"]["beauty"]["male_score"]
+                   faces["gender"] = "男性"
+               else:
+                   faces["beauty"] = face["attributes"]["beauty"]["female_score"]
+                   faces["gender"] = "女性"
+               faces["x_axis"] = face["face_rectangle"]["left"] # 並び替え用に画像上の顔のX座標の位置を代入
+               face_list.append(faces)
+       face_list = sorted(face_list, key=lambda x:x["x_axis"]) # 左から順に並び変える
+       msg = ""
+       for i, f in enumerate(face_list, 1):
+           msg += "{}人目の情報\n".format(i)
+           # msg += "X軸の位置:{} \n".format(face["x_axis"]) デバッグ用
+           msg += "性別: {}\n".format(f["gender"])
+           msg += "年齢: {}歳\n".format(f["age"])
+           # 人種を取得したい場合は以下の記述を追加する
+           # msg += "人種: {}\n".format(f["ethnicity"])
+           msg += "偏差値: {}\n\n".format(int(f["beauty"]))
+       msg = msg.rstrip()
+       if not msg:
+           msg = "画像から顔データを検出できませんでした。"
+       return msg
+   except:
+       return "サーバーの接続に失敗したか画像を正しく認識できませんでした。"
--- main.py
+++ main.py
@@ -1,53 +1,69 @@
 from flask import Flask, request, abort
 import os
 
+import face_detect as f  # face_detect.py
+import base64
+
 from linebot import (
-    LineBotApi, WebhookHandler
+   LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
-    InvalidSignatureError
+   InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+   MessageEvent, TextMessage, TextSendMessage, ImageMessage #ImageMessageを追加
 )
 
 app = Flask(__name__)
 
-#環境変数取得
+# 環境変数取得
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
 YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
+
 @app.route("/")
 def hello_world():
-    return "hello world!"
+   return "hello world!"
+
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
+   # get X-Line-Signature header value
+   signature = request.headers['X-Line-Signature']
 
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+   # get request body as text
+   body = request.get_data(as_text=True)
+   app.logger.info("Request body: " + body)
 
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
+   # handle webhook body
+   try:
+       handler.handle(body, signature)
+   except InvalidSignatureError:
+       abort(400)
 
-    return 'OK'
+   return 'OK'
+
 
 @handler.add(MessageEvent, message=TextMessage)
+# テキストの場合はオウム返し
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+   line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
+
+@handler.add(MessageEvent, message=ImageMessage)
+def handle_image_message(event):
+   push_img_id = event.message.id # 投稿された画像IDを取得
+   message_content = line_bot_api.get_message_content(push_img_id) # LINEサーバー上に自動保存された画像を取得
+   push_img = b""
+   for chunk in message_content.iter_content(): 
+       push_img += chunk #画像をiter_contentでpush_imgに順次代入
+   push_img = base64.b64encode(push_img) # APIに通すためbase64エンコード
+   msg = f.face_detect(push_img)
+   line_bot_api.reply_message(event.reply_token, TextSendMessage(text=msg))
 
 if __name__ == "__main__":
-#    app.run()
-    port = int(os.getenv("PORT"))
-    app.run(host="0.0.0.0", port=port)
+   #    app.run()
+   port = int(os.getenv("PORT"))
+   app.run(host="0.0.0.0", port=port)
